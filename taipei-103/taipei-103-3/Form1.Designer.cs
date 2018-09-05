c544d79e19de1b45b4371d649b9491d70822ecb6
Wind_ContestantPC <shiow620412@gmail.com>
2018-09-05 13:10:45 +0800

    upload

---
+++ taipei-103/taipei-103-3/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace taipei_103_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "25,25,26,28,27,29,30,34,37,36,37,40";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(415, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "23,24,25,26,28,29,30,35,37,38,39";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 89);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(546, 372);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "計算DTW累積距離";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ taipei-103/taipei-103-3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taipei_103_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] input1 = Array.ConvertAll(textBox2.Text.Split(','),s=>int.Parse(s));
+            int[] input2 = Array.ConvertAll(textBox1.Text.Split(','),s=>int.Parse(s));
+            int[,] output = new int[input1.Length, input2.Length];
+            for(int i = 0; i < output.GetLength(0);i++)
+            {
+                for(int j =0; j< output.GetLength(1);j++)
+                {
+                    if (i == 0 && j == 0)
+                        output[i, j] = Math.Abs(input1[i] - input2[j]);
+                    else if (i == 0 && j > 0)
+                        output[i, j] = Math.Abs(input1[i] - input2[j]) + output[0, j - 1];
+                    else if (i > 0 && j == 0)
+                        output[i, j] = Math.Abs(input1[i] - input2[j]) + output[i - 1, 0];
+                    else
+                        output[i, j] = Math.Abs(input1[i] - input2[j]) + Math.Min(Math.Min(output[i - 1, j] ,output[i, j - 1] ), output[i - 1, j - 1]);
+                }
+            }
+            textBox3.Clear();
+            textBox3.Text += "0\t";
+            for (int i = 0; i < input2.Length; i++)
+                textBox3.Text += input2[i] + "\t";
+            textBox3.Text += "\r\n";
+            for (int i = 0; i < output.GetLength(0); i++)
+            {
+                textBox3.Text += input1[i]+"\t";
+                for (int j = 0; j < output.GetLength(1); j++)
+                {
+                    textBox3.Text += output[i, j]+ "\t";
+                }
+                textBox3.Text += "\r\n";
+            }
+
+        }
+    }
+}
