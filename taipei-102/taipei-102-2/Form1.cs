0d702489ba7611246eb52779b49b937f86978062
Wind_ContestantPC <shiow620412@gmail.com>
2018-09-06 13:05:03 +0800

    upload

--- taipei-102/taipei-102-2/Form1.cs
+++ taipei-102/taipei-102-2/Form1.cs
@@ -36,13 +36,36 @@ namespace taipei_102_2
             double a = Math.Sqrt(Math.Pow( p[0].X - p[1].X , 2 )+ Math.Pow(p[0].Y - p[1].Y, 2));
             double b = Math.Sqrt(Math.Pow(p[2].X - p[1].X, 2) + Math.Pow(p[2].Y - p[1].Y, 2));
             double c = Math.Sqrt(Math.Pow(p[0].X - p[2].X, 2) + Math.Pow(p[0].Y - p[2].Y, 2));
+            a = Math.Round(a,3);
+            b = Math.Round(b,3);
+            c = Math.Round(c,3);
             label4.Text = $"座標1~座標2 邊長={a}{Environment.NewLine}座標2~座標3 邊長={b}{Environment.NewLine}座標3~座標1 邊長={c}";
             double m1 = (p[1].Y - p[0].Y) / (p[1].X - p[0].X);
             double m2 = (p[2].Y - p[1].Y) / (p[2].X - p[1].X);
-            if (m1 == m2)
-            {
+            if ((p[0].X == p[1].X && p[0].Y == p[1].Y) | (p[2].X == p[1].X && p[2].Y == p[1].Y) | (p[0].X == p[2].X && p[0].Y == p[2].Y))
+                label5.Text = "有相同點座標";
+            else if (m1 == m2)
                 label5.Text = "此為三點一直線";
-            }
+            else if (a == b && b == c)
+                label5.Text = "此為正三角形";
+            else if ((Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Round(Math.Pow(c, 2)) |
+                      Math.Pow(c, 2) + Math.Pow(b, 2) == Math.Round(Math.Pow(a, 2)) |
+                      Math.Pow(c, 2) + Math.Pow(a, 2) == Math.Round(Math.Pow(b, 2))) && (a == b | b == c | c == a))
+                label5.Text = "此為等腰直角三角形";
+            else if (a == b | b == c | c == a)
+                label5.Text = "此為等腰三角形";
+            else if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Round(Math.Pow(c, 2))|
+                      Math.Pow(c, 2) + Math.Pow(b, 2) ==Math.Round(Math.Pow(a, 2)) |
+                      Math.Pow(c, 2) + Math.Pow(a, 2) == Math.Round(Math.Pow(b, 2)))
+                label5.Text = "此為直角三角形";
+            else if (Math.Pow(a, 2) + Math.Pow(b, 2) < Math.Pow(c, 2) |
+                      Math.Pow(c, 2) + Math.Pow(b, 2) < Math.Pow(a, 2) |
+                      Math.Pow(c, 2) + Math.Pow(a, 2) < Math.Pow(b, 2))
+                label5.Text = "此為鈍角三角形";
+            else if (Math.Pow(a, 2) + Math.Pow(b, 2) > Math.Pow(c, 2) |
+                      Math.Pow(c, 2) + Math.Pow(b, 2) > Math.Pow(a, 2) |
+                      Math.Pow(c, 2) + Math.Pow(a, 2) > Math.Pow(b, 2))
+                label5.Text = "此為銳角三角形";
 
         }
     }
