1b60b6850aabf166f22371c52e11a711a2b4915e
WindLin <shiow620412@github.com>
2018-09-09 19:14:24 +0800

    finish
    台北市軟體競賽99-106屆完成
    105-3缺乏測資故略過

---
+++ taipei-99/taipei-99-4-2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taipei_99_4_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Node
+        {
+            public int[] Status;
+            public Node Father;
+            public Node(int[] Status,Node Father)
+            {
+                this.Status = Status;
+                this.Father = Father;
+            }
+            public bool IsCorrect()
+            {
+                for (int i = 0; i < Status.Length; i++)
+                    if (Status[i] < 0)
+                        return false;
+                if ((Status[0] > Status[1] && Status[1] > 0) | (Status[3] > Status[4] && Status[4] > 0))
+                    return false;
+                return true;
+            }
+            public bool boatInLeft() => Status[2] == 1;
+            public Node MoveLtoR(int m , int p)
+            {
+                int[] arr = (int[])Status.Clone();
+                arr[0] -= m;
+                arr[1] -= p;
+                arr[2] -= 1;
+                arr[3] += m;
+                arr[4] += p;
+                arr[5] += 1;
+                Node next = new Node(arr, this);
+                if (next.IsCorrect())
+                    return next;
+                else
+                    return null;
+
+            }
+            public Node MoveRtoL(int m, int p)
+            {
+                int[] arr = (int[])Status.Clone();
+                arr[0] += m;
+                arr[1] += p;
+                arr[2] += 1;
+                arr[3] -= m;
+                arr[4] -= p;
+                arr[5] -= 1;
+                Node next = new Node(arr, this);
+                if (next.IsCorrect())
+                    return next;
+                else
+                    return null;
+
+            }
+            public string Sequence()
+            {
+                return string.Join("", Array.ConvertAll(Status, s=>s.ToString() ));
+            }
+            public override string ToString()
+            {
+                string str = "";
+                if (boatInLeft())
+                {
+                    str = $"右岸->左岸 傳教士過去 {Father.Status[4]-Status[4]} , 野人過去 {Father.Status[3] - Status[3]}";
+                }
+                else
+                {
+                    str = $"左岸->右岸 傳教士過去 {Father.Status[1] - Status[1]} , 野人過去 {Father.Status[0] - Status[0]}";
+                }
+                return $"{str}\t 左岸( 野{Status[0]} 傳{Status[1]} 船{Status[2]} ) 右岸( 野{Status[3]} 傳{Status[4]} 船{Status[5]} )";
+            }
+        }
+        List<Node> getNext(Node n)
+        {
+            List<Node> ln = new List<Node>();
+            if (n.boatInLeft())
+            {
+                ln.Add(n.MoveLtoR(1, 1));
+                ln.Add(n.MoveLtoR(0, 1));
+                ln.Add(n.MoveLtoR(0, 2));
+                ln.Add(n.MoveLtoR(1, 0));
+                ln.Add(n.MoveLtoR(2, 0));
+            }
+            else
+            {
+                ln.Add(n.MoveRtoL(1, 1));
+                ln.Add(n.MoveRtoL(0, 1));
+                ln.Add(n.MoveRtoL(0, 2));
+                ln.Add(n.MoveRtoL(1, 0));
+                ln.Add(n.MoveRtoL(2, 0));
+            }
+            return ln;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int[] input = Array.ConvertAll(textBox1.Text.Split(','), int.Parse);
+            Node start = new Node(input, null);            
+            if (!start.IsCorrect())
+            {
+                MessageBox.Show("違反題意");
+                return;
+            }
+            Queue<Node> q = new Queue<Node>();
+            SortedList<string, bool> book = new SortedList<string, bool>();
+            book.Add(start.Sequence(), true);
+            Node end = new Node(new int[] { 0, 0, 0, 3, 3, 1 }, null);
+            string endSequence = end.Sequence();
+            q.Enqueue(start);
+            while (q.Count>0)
+            {
+                Node now = q.Dequeue();
+                if(now.Sequence() == endSequence)
+                {
+                    Stack<Node> output = new Stack<Node>();
+                    while(now.Father!=null)
+                    {
+                        output.Push(now);
+                        now = now.Father;
+                    }
+                    while (output.Count > 0)
+                        listBox1.Items.Add(output.Pop().ToString());
+                    MessageBox.Show("成功過河");
+                    return;
+                }
+                List<Node> next = getNext(now);
+                foreach(var n in next)
+                {
+                    if (n==null) continue;
+                    if (!book.Keys.Contains(n.Sequence()))
+                    {
+                        q.Enqueue(n);
+                        book.Add(n.Sequence(), true);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ taipei-99/taipei-99-4/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taipei_99_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Status
+        {
+            public int[] status;
+            public int father;
+            public int index;
+            public Status(int[] status,int father,int index)
+            {
+                this.status = status;
+                this.father = father;
+                this.index = index;
+                
+            }
+            public Status()
+            {
+
+            }
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Status> s = new List<Status>();
+            s.Add(new Status(Array.ConvertAll(textBox1.Text.Split(','), int.Parse), 0,0));
+            Status ans=new Status(), temp;
+            Queue<Status> q = new Queue<Status>();
+            int[] i;
+            q.Enqueue(s[0]);
+            book.Add(s[0].status);
+            bool getAns = false;
+            while( q.Count>0 &&!getAns)
+            {
+                temp = q.Dequeue();
+                i = temp.status;
+                if(i[3]==3 && i[4] == 3 && i[5] == 1)
+                {
+                    ans = temp;
+                    getAns = true;
+                    break;
+                }
+                if (temp.status[2]==1)
+                {                   
+                    if (check(new int[] { i[0]-1, i[1]-1, i[2]-1,
+                                          i[3]+1, i[4]+1, i[5]+1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] - 1, i[1] - 1, i[2] - 1,
+                                                     i[3] + 1, i[4] + 1, i[5] + 1 }, temp.index, s.Count));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0]-1, i[1], i[2]-1,
+                                          i[3]+1, i[4], i[5]+1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] - 1, i[1] , i[2] - 1,
+                                                     i[3] + 1, i[4] , i[5] + 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0], i[1]-1, i[2]-1,
+                                          i[3], i[4]+1, i[5]+1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] , i[1] - 1, i[2] - 1,
+                                                     i[3] , i[4] + 1, i[5] + 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0]-2, i[1], i[2]-1,
+                                          i[3]+2, i[4], i[5]+1 }))
+                    {
+                         s.Add(new Status(new int[] { i[0] - 2, i[1] , i[2] - 1,
+                                                      i[3] + 2, i[4] , i[5] + 1 }, temp.index, s.Count));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0], i[1]-2, i[2]-1,
+                                          i[3], i[4]+2, i[5]+1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] , i[1] - 2, i[2] - 1,
+                                                     i[3] , i[4] + 2, i[5] + 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                }
+                else
+                {
+                    if (check(new int[] { i[0]+1, i[1]+1, i[2]+1,
+                                          i[3]-1, i[4]-1, i[5]-1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] + 1, i[1] + 1, i[2] + 1,
+                                                     i[3] - 1, i[4] - 1, i[5] - 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0]+1, i[1], i[2]+1,
+                                          i[3]-1, i[4], i[5]-1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] + 1, i[1] , i[2] + 1,
+                                                     i[3] - 1, i[4] , i[5] - 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0], i[1]+1, i[2]+1,
+                                          i[3], i[4]-1, i[5]-1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] , i[1] + 1, i[2] + 1,
+                                                     i[3] , i[4] - 1, i[5] - 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0]+2, i[1], i[2]+1,
+                                          i[3]-2, i[4], i[5]-1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] + 2, i[1] , i[2] + 1,
+                                                     i[3] - 2, i[4] , i[5] - 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                    if (check(new int[] { i[0], i[1]+2, i[2]+1,
+                                          i[3], i[4]-2, i[5]-1 }))
+                    {
+                        s.Add(new Status(new int[] { i[0] , i[1] + 2, i[2] + 1,
+                                                     i[3] , i[4] - 2, i[5] - 1 }, temp.index, s.Count ));
+                        q.Enqueue(s.Last());
+                    }
+                }
+            }
+            int[] tempInt = new int[6];
+            List<string> output = new List<string>();
+            int a = 0, b = 0;
+            q.Clear();
+            q.Enqueue(ans);
+            while(q.Count>0)
+            {
+                temp = q.Dequeue();
+                tempInt = temp.status;                
+                //右岸至左岸                
+                if(tempInt[2] ==1)
+                {
+                    a = Math.Abs(tempInt[3] - s[temp.father].status[3]);
+                    b = Math.Abs(tempInt[4] - s[temp.father].status[4]);
+                    output.Add($"右岸->左岸 傳教士過去 {a} ,野人過去 {b}        左岸(野{tempInt[0]}傳{tempInt[1]}船{tempInt[2]})        右岸(野{tempInt[3]}傳{tempInt[4]}船{tempInt[5]}) \r\n");
+                }
+                else
+                {
+                    a = Math.Abs(tempInt[0] - s[temp.father].status[0]);
+                    b = Math.Abs(tempInt[1] - s[temp.father].status[1]);
+                    output.Add($"左岸->右岸 傳教士過去 {a} ,野人過去 {b}        左岸(野{tempInt[0]}傳{tempInt[1]}船{tempInt[2]})        右岸(野{tempInt[3]}傳{tempInt[4]}船{tempInt[5]}) \r\n");
+                }
+                if (temp.father != 0)
+                    q.Enqueue(s[temp.father]);
+            }          
+            output.Reverse();
+            textBox2.Clear();
+            foreach(var str in output)
+            {
+                textBox2.Text += str;
+            }
+            
+
+            
+
+               
+        }
+        List<int[]> book = new List<int[]>();
+        bool check(int[] i)
+        {
+            bool notRepeat = true;
+            book.ForEach(x =>
+            {
+                if (x == i)
+                    notRepeat= false;
+            });
+            if ((i[0] > i[1] && i[1]>0 )| (i[3] > i[4] && i[4]>0)|i[0]<0 | i[1] < 0 | i[2] < 0 | i[3] < 0 | i[4] < 0 | i[5] < 0)
+                notRepeat = false;
+            if (notRepeat)
+                book.Add(i);
+            return notRepeat;
+        }
+    }
+}
