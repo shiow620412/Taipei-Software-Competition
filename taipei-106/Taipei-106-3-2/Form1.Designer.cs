aa466fec54d4a74f339a688cfc6e308fd6a3cd2b
WindLin <shiow620412@github.com>
2018-10-31 23:57:03 +0800

    upload
    完成106-3 所有功能

---
+++ taipei-106/Taipei-106-3-2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Taipei_106_3_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(947, 575);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(315, 641);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "是否顯示格線";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "座標";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 627);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "讀檔";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 627);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "執行內插";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(214, 627);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "清除螢幕";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 683);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ taipei-106/Taipei-106-3-2/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Taipei_106_3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        int scale = 10;
+        void rePaint()
+        {
+         
+            Bitmap bmp = new Bitmap(1600, 880);
+            Graphics g = Graphics.FromImage(bmp);
+            for (int j = 0; j < bmp.Height; j++)
+            {
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    if (i % 5 == 0 && j % 5 == 0)
+                        bmp.SetPixel(i, j, Color.Black);
+                    if (i % 5 == 0 && j == 0 && checkBox1.Checked)
+                        g.DrawLine(Pens.Black, i, 0, i, bmp.Height);
+                }
+                if (j % 5 == 0 && checkBox1.Checked)
+                    g.DrawLine(Pens.Black, 0, j, bmp.Width, j);
+            }
+            pictureBox1.Image = bmp;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Width = 1600*scale;
+            pictureBox1.Height = 880*scale;
+            rePaint();            
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Focus();
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            bool line = ((CheckBox)sender).Checked;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Graphics g = Graphics.FromImage(bmp);
+            
+            
+            for (int i = 0; i < bmp.Width; i++)                 
+                if (i % 5 ==  0 && line)
+                    g.DrawLine(Pens.Black, i, 0, i, bmp.Height);       
+            for (int i = 0; i < bmp.Height; i++)
+                if (i % 5 == 0 && line)
+                    g.DrawLine(Pens.Black, 0,i, bmp.Width, i);
+            if(!line)
+            for (int j = 0; j < bmp.Height; j++)
+            {
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    if (i % 5 != 0 | j % 5 != 0)
+                        bmp.SetPixel(i, j, Color.White);
+                   
+                }
+            }
+            pictureBox1.Image = bmp;
+            
+
+
+        }
+        List<PointF> pts = new List<PointF>();
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = $"座標({e.X/scale},{e.Y/scale})";
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+ 
+            bmp.SetPixel(e.X/ scale, e.Y/ scale, Color.Red);
+            pts.Add(new Point(e.X / scale, e.Y / scale));
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rePaint();
+            pts.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            pts.Clear();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] input = File.ReadAllLines(ofd.FileName);
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Graphics g = Graphics.FromImage(bmp);
+                for (int i = 1; i < input.Length; i++)
+                {
+                    PointF p = new PointF();
+                    string[] point = input[i].Split(' ');
+                    p.X = float.Parse(point[0]) ;
+                    p.Y = float.Parse(point[1]) ;
+                    bmp.SetPixel((int)p.X, (int)p.Y, Color.Red);
+                    g.DrawEllipse(Pens.Red, new Rectangle((int)p.X - 2, (int)p.Y - 2, 2, 2));
+                    pts.Add(p);
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int min = (int)pts.Min(x => x.X);
+            int max = (int)pts.Max(x => x.X);
+            Bitmap bmp = new Bitmap(1600,880);
+            Graphics g = Graphics.FromImage(bmp);
+            List<Point> p = new List<Point>();
+            for (int j = 0; j < bmp.Height; j++)
+            {
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    if (i % 5 == 0 && j % 5 == 0)
+                        bmp.SetPixel(i, j, Color.Black);
+                    if (i % 5 == 0 && j == 0 && checkBox1.Checked)
+                        g.DrawLine(Pens.Black, i, 0, i, bmp.Height);
+                }
+                if (j % 5 == 0 && checkBox1.Checked)
+                    g.DrawLine(Pens.Black, 0, j, bmp.Width, j);
+            }
+            for (int i = 0; i < pts.Count; i++)
+                g.DrawEllipse(Pens.Red, new Rectangle((int)pts[i].X - 2, (int)pts[i].Y - 2, 2, 2));
+            for (int i = 0; i <= 1599; i++)
+            {
+                int result = 0;
+                for (int m = 0; m < pts.Count; m++)
+                {
+                    float up = 1;
+                    float down = 1;
+                    for (int n = 0; n < pts.Count; n++)
+                    {
+                        if (m == n)
+                            continue;
+                        up *= i - pts[n].X;
+                        down *= pts[m].X - pts[n].X;
+                    }
+                    result += (int)(pts[m].Y * up / down);
+                }
+                if (result < 880 && result > -1)
+                {
+                    bmp.SetPixel(i, result, Color.Red);
+                    p.Add(new Point(i, result));
+                }
+
+            }
+            g.DrawLines(Pens.Red, p.ToArray());
+            pictureBox1.Image = bmp;
+        }
+    }
+}
